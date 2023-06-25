6e50aa45d30347502e772c789dd7c05e3e6b6958
Jhonatan Sampaio <jhonaatansampaio@hotmail.com>
2023-06-25 18:43:05 -0300

    #7 add page contato

--- frontend/src/app/components/pages/contact/contact.component.ts
+++ frontend/src/app/components/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  mensagem = {
+    nome: '',
+    email: '',
+    assunto: '',
+    texto: ''
+  };
+  constructor(private http: HttpClient) {}
 
+  enviarMensagem() {
+    this.http.post('/enviar-email', this.mensagem)
+      .subscribe(
+        response => {
+          console.log('Mensagem enviada com sucesso!', response);
+
+        },
+        error => {
+          console.error('Erro ao enviar mensagem:', error);
+
+        }
+      );
+  }
 }
